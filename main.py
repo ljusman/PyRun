d0261bf0d5ec7da3fe76cf18c8391b76ab1890af
Mark Bahnman <mark.bahnman@gmail.com>
2013-03-05 23:57:23 -0800

    Implemented startScreen functionality

---
+++ main.py
@@ -0,0 +1,86 @@
+import random, copy, os, pygame, sys
+from pygame.locals import *
+
+FPS = 30 # frames per second to update the screen
+WINWIDTH = 800 # width of the program's window, in pixels
+WINHEIGHT = 600 # height in pixels
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+CAM_MOVE_SPEED = 5 # how many pixels per frame the camera moves
+
+BRIGHTBLUE  = (  0, 170, 255)
+WHITE       = (255, 255, 255)
+BGCOLOR     = BRIGHTBLUE
+TEXTCOLOR   = WHITE
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, IMAGESDICT, BASICFONT, PLAYERIMAGES, currentImage
+    # Pygame initialization and basic set up of the globalvariables
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+
+    DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+
+    pygame.display.set_caption('PyRun')
+    BASICFONT = pygame.font.Font('freesansbold.ttf',18)
+
+    # This is a global Dict object (or dictionary object) which
+    # contains all of the images that we will use in the game
+    IMAGESDICT = {'title': pygame.image.load('title.png')}
+
+    # PLAYERIMAGES is a list of all possible characters the player can be.
+    # currentImage is the index of the player's current player image.
+    currentImage = 0
+    # PLAYERIMAGES = [IMAGESDICT['princess']]
+    
+    startScreen() # function which shows the start menu
+
+def startScreen():
+    # Position the title image.
+    titleRect = IMAGESDICT['title'].get_rect()
+    topCoord = 50 # topCoord track where to position the top of the text
+    titleRect.top = topCoord
+    titleRect.centerx = HALF_WINWIDTH
+    topCoord += titleRect.height
+
+    # Unfortunately Pygame's font and text system only show one line at
+    # a time, so we can't use string with the \n newline characters in them.
+    # So we will use a list with each line in it,
+    instructionText = ['Arrow keys or WASD to move',
+                        'Esc to quit.']
+
+    # Star with drawing a black color to the entire window
+    DISPLAYSURF.fill(BGCOLOR)
+
+    #Draw the title image to the window:
+    DISPLAYSURF.blit(IMAGESDICT['title'], titleRect)
+
+    # Position and draw the text.
+    for i in range(len(instructionText)):
+        instSurf = BASICFONT.render(instructionText[i], 1, TEXTCOLOR)
+        instRect = instSurf.get_rect()
+        topCoord += 10 # 10 pixels will go in between each line of text.
+        instRect.top = topCoord
+        instRect.centerx = HALF_WINWIDTH
+        topCoord += instRect.height # Adjust for the height of the line.
+        DISPLAYSURF.blit(instSurf, instRect)
+
+    while True: # Main loop for the start screen.
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                return # user has pressed a key, so return.
+
+        # Display the DISPLAYSURF contents to the actual screen.
+        pygame.display.update()
+        FPSCLOCK.tick()
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
