c6eeff6de75c64eb3f3171fe7fea776e376d6321
GIT Lukas <ljusman@sfu.ca>
2013-03-24 15:58:58 -0700

    Added structures for various AI, and implemented banana peel AI

--- AI.py
+++ AI.py
@@ -47,10 +47,77 @@ class spikes(stationaryObstacle):
     def __init__(self,pos,size,image):
         return stationaryObstacle.__init__(self, pos, size, image)
 
-# Obstacles capable of moving
-class movingObstacle(Obstacle):
+class treeLog(stationaryObstacle):
 
     def __init__(self,pos,size,image):
+        return stationaryObstacle.__init__(self, pos, size, image)
+
+# Obstacles that are set into motion when triggered
+class triggeredObstacle(Obstacle):
+
+    def __init__(self,pos,size,image):
+        self.triggerDelay = 0
+        return Obstacle.__init__(self, pos, size, image)
+
+    def move(self, xIncrement, yIncrement): 
+        self.xPos += xIncrement
+        self.yPos += yIncrement
+
+class bananaPeel(triggeredObstacle):
+
+    def __init__(self,pos,size,image):
+        self.xSpeed = 2.5
+        self.ySpeed = 2.5
+        self.rotation = 0
+        self.slippedOn = False
+        # Properties of time concerned with the last slip with this object
+        self.slipTimeCounter = 0
+        self.slipRiseTime = 50
+        # Properties of gravity that dictate the banana peel's triggered movement
+        self.gravityForce = 1
+        self.gravityXCarry = -1
+        return triggeredObstacle.__init__(self, pos, size, image)
+
+    def slipRotate(self, gameFloor, rotateIncrementInit, rotateIncrementFall):
+        if (self.slipTimeCounter < self.slipRiseTime and self.slippedOn):
+            self.rotation += rotateIncrementInit
+            return (self.rotation)
+        else:
+            # Did we already hit the floor?
+            if (self.yPos >= gameFloor):
+                self.rotation = 0
+            else:
+                self.rotation += rotateIncrementFall                
+            return (self.rotation)
+
+    def doBananaPeelGravity(self, obj, floor, downAccel):
+        if self.yPos < floor:
+            self.move(self.gravityXCarry, 0)
+            # Settings on the gravity, so we have acceleration.
+            self.gravityForce += (self.gravityForce * downAccel)
+            self.move(0, self.gravityForce)
+
+    def doBananaPeelAction(self, obj, gameFloor, gravAccel, winWidth):
+        if (self.isTouching(obj.x, obj.y, obj.x + obj.width, obj.y + obj.height) and self.slippedOn == False):
+            self.slippedOn = True
+
+        if (self.slippedOn and self.slipTimeCounter < self.slipRiseTime):            
+            self.slipTimeCounter += 1            
+            self.move(-self.xSpeed, -self.ySpeed)
+        else:
+            # This is the point where the banana peel reaches its maximum height
+            if (self.slipTimeCounter >= self.slipRiseTime):                
+                self.doBananaPeelGravity(obj, gameFloor, gravAccel)                
+                if (self.yPos >= gameFloor):
+                    self.slippedOn = False
+                    self.slipTimeCounter = 0
+                    self.gravityForce = 1
+                    
+
+# Obstacles capable of moving on their own
+class movingObstacle(Obstacle):
+
+    def __init__(self,pos,size,image):        
         return Obstacle.__init__(self, pos, size, image)
 
     # Accepts a surface image to flip. "hori" and "vert" are booleans.
@@ -92,6 +159,8 @@ class soccerBall(movingObstacle):
             # Settings on the gravity, so we have acceleration.
             self.gravityForce += (self.gravityForce * downAccel)
             self.move(0, self.gravityForce)
+        else:
+            self.gravityForce = 1
 
     def doSoccerBallAction(self, obj, gameFloor, gravAccel, winWidth):        
 
