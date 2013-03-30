251a89658a0b1b418a9ef123a36f687cc0e0a366
GIT Lukas <ljusman@sfu.ca>
2013-03-29 17:22:23 -0700

    New AI creation and image transparency functionality. More AI incorporation.

--- main.py
+++ main.py
@@ -25,18 +25,22 @@ JUMP_HEIGHT = 200           # pixels
 
 
 # Here is the place to define constants for AI implementation...
-SOCCER_BALL_POSITION = ((WINWIDTH - 100), HALF_WINHEIGHT - 300)
+SOCCER_BALL_POSITION = ((WINWIDTH - 100), (HALF_WINHEIGHT - 100))
 SOCCER_BALL_SIZE = (16, 16)
 SOCCER_GRAVITY = 0.01
 SOCCER_FLOOR_ADJUSTMENT_FACTOR = 2.6
 SOCCER_ROTATE_INCREMENT = 2
 aiMoveStarted = False
 
-BANANA_PEEL_POSITION = ((WINWIDTH - 100), HALF_WINHEIGHT)
-BANANA_PEEL_SIZE = (64, 64)
+BANANA_PEEL_POSITION = ((WINWIDTH - 100), (HALF_WINHEIGHT))
+BANANA_PEEL_SIZE = (50, 50)
 BANANA_PEEL_INIT_SLIP_TIME = 0
 BANANA_ROTATE_FIRST = -2
 BANANA_ROTATE_SECOND = 0
+BANANA_PEEL_FADE_DECREMENT = -4
+
+SPIKES_POSITION = ((WINWIDTH - 200), (HALF_WINHEIGHT - 3))
+SPIKES_SIZE = (128, 50)
 
 def floorY():
     ''' The Y coordinate of the floor, where the man is placed '''
@@ -47,6 +51,34 @@ def jumpHeightAtTime(elapsedTime):
     return ((-1.0/TIME_AT_PEAK**2)* \
         ((elapsedTime-TIME_AT_PEAK)**2)+1)*JUMP_HEIGHT
 
+'''
+    Use this method for blitting images that are suppposed to be partially
+    or wholly transparent. Apparently, Python does not provide a good
+    method (not even set_alpha(), nor convert()/convert_alpha() in
+    conjunction with this works) for blitting these types of images.
+    You can have a look here:
+    http://www.nerdparadise.com/tech/python/pygame/blitopacity/
+'''
+def blit_alpha(screenSurface, source, location, opacity):
+        x = location[0]
+        y = location[1]
+        temp = pygame.Surface((source.get_width(), source.get_height())).convert()
+        temp.blit(screenSurface, (-x, -y))
+        temp.blit(source, (0, 0))
+        temp.set_alpha(opacity)        
+        screenSurface.blit(temp, location)
+
+def makeObstacle(obstacleChoice, position, size, image, direction = 'left'):    
+    if (obstacleChoice == 'Spikes'):
+        return AI.spikes(position, size, image)
+    elif (obstacleChoice == 'Giant rock'):
+        return AI.soccerBall(position, size, image, direction)
+    elif (obstacleChoice == 'Banana peel'):
+        return AI.bananaPeel(position, size, image)
+    else:
+        return AI.Obstacle((0,0), (0,0), pygame.Surface((0, 0)))
+            
+    
 def main():
     global FPSCLOCK, DISPLAYSURF, IMAGESDICT, BASICFONT, PLAYERIMAGES, currentImage
     # Pygame initialization and basic set up of the globalvariables
@@ -63,8 +95,9 @@ def main():
     IMAGESDICT = {
         'title': pygame.image.load('img/title.png'),
         'player': pygame.image.load('img/princess.png'),
+        'spikes': pygame.image.load('img/spikes.png'),
         'soccerAI': pygame.image.load('img/soccer_ball.png'),
-        'banana_peel': pygame.image.load('img/banana_peel.png')
+        'banana_peel': pygame.image.load('img/peel.png')        
         }    
 
     # PLAYERIMAGES is a list of all possible characters the player can be.
@@ -78,7 +111,8 @@ def main():
 
 def runGame():
 
-    ''' set up initial player object
+    '''
+        Set up initial player object
         This object contains the following keys:
             surface: the image of the player
             facing: the direction the player is facing
@@ -94,25 +128,19 @@ def runGame():
         IMAGESDICT['player']
         )
 
-    SOCCER_IMG_SCALE = pygame.transform.scale(IMAGESDICT['soccerAI'], SOCCER_BALL_SIZE)
-    BANANA_IMG_SCALE = pygame.transform.scale(IMAGESDICT['banana_peel'], BANANA_PEEL_SIZE)
-    # Initialize the soccer ball object
-    soccerBall = AI.soccerBall(        
-        SOCCER_BALL_POSITION,
-        SOCCER_BALL_SIZE,
-        SOCCER_IMG_SCALE,
-        LEFT
-        )
+    SOCCER_IMG_SCALE = pygame.transform.smoothscale(IMAGESDICT['soccerAI'], SOCCER_BALL_SIZE)
+    BANANA_IMG_SCALE = pygame.transform.smoothscale(IMAGESDICT['banana_peel'], BANANA_PEEL_SIZE) 
+    SPIKES_IMG_SCALE = pygame.transform.smoothscale(IMAGESDICT['spikes'], SPIKES_SIZE)
+    
+    # For storing our obstacles
+    obstacleObjs = []
 
-    # Initialize the banana peel object
-    bananaPeel = AI.bananaPeel(
-        BANANA_PEEL_POSITION,
-        BANANA_PEEL_SIZE,
-        BANANA_IMG_SCALE
-        )
+    # Append a soccer ball and banana peel object to our list of objects
+    obstacleObjs.append(makeObstacle('Giant rock', SOCCER_BALL_POSITION, SOCCER_BALL_SIZE, SOCCER_IMG_SCALE))
+    obstacleObjs.append(makeObstacle('Spikes', SPIKES_POSITION, SPIKES_SIZE, SPIKES_IMG_SCALE))    
 
     slipTimeElapsed = BANANA_PEEL_INIT_SLIP_TIME
-
+    
     moveLeft  = False
     moveRight = False
     moveUp    = False
@@ -176,25 +204,49 @@ def runGame():
             #p.y += MOVERATE
             pass
 
-        # Preliminaries of soccer ball AI
-        soccerBall.doSoccerBallAction(p, floorY() + (p.height/SOCCER_FLOOR_ADJUSTMENT_FACTOR), SOCCER_GRAVITY, WINWIDTH)
-        bananaPeel.doBananaPeelAction(p, floorY(), SOCCER_GRAVITY, WINWIDTH)        
-        ##################################
-        
-
         # Draw the background
         DISPLAYSURF.fill(BGCOLOR)
 
         # Draw the player
         DISPLAYSURF.blit(p.image, p.get_rect())
 
-        # Draw the soccer ball AI
-        SOCCER_IMG_ROT = pygame.transform.rotate(soccerBall.image, soccerBall.soccerBallRotate(SOCCER_ROTATE_INCREMENT))
-        DISPLAYSURF.blit(SOCCER_IMG_ROT, soccerBall.get_rect())
-        # Draw the banana peel AI
-        BANANA_IMG_ROT = pygame.transform.rotate(bananaPeel.image, bananaPeel.slipRotate(floorY(), BANANA_ROTATE_FIRST, BANANA_ROTATE_SECOND))
-        DISPLAYSURF.blit(BANANA_IMG_ROT, bananaPeel.get_rect())
-    
+        ''' Collision debugging '''
+        # pygame.draw.rect(DISPLAYSURF, (0, 0, 0), (p.x, p.y, p.width, p.height))
+        # pygame.draw.rect(DISPLAYSURF, (255, 255, 255), (obstacleObjs[0].xPos, obstacleObjs[0].yPos, obstacleObjs[0].width, obstacleObjs[0].height))
+        # pygame.draw.rect(DISPLAYSURF, (255, 0, 255), (obstacleObjs[1].xPos, obstacleObjs[1].yPos, obstacleObjs[1].width, obstacleObjs[1].height))
+
+        '''
+            We need specific drawing cases for different obstacles,
+            since every obstacle could have different methods
+            defined for drawing executions. This is what we do
+            below.
+        '''
+        '''
+            Here, we have backwards-list checking to avoid a common object
+            deletion mistake.
+        '''    
+        for i in range(len(obstacleObjs) - 1, -1, -1):            
+            # Checking if a particular object is a soccer ball.
+            if isinstance(obstacleObjs[i], AI.soccerBall):                
+                obstacleObjs[i].doSoccerBallAction(p, floorY() + (p.height/SOCCER_FLOOR_ADJUSTMENT_FACTOR), SOCCER_GRAVITY, WINWIDTH)
+                SOCCER_IMG_ROT = pygame.transform.rotate(obstacleObjs[i].image, obstacleObjs[i].soccerBallRotate(SOCCER_ROTATE_INCREMENT))
+                DISPLAYSURF.blit(SOCCER_IMG_ROT, obstacleObjs[i].get_rect())
+            # Checking if a particular object is a banana peel.
+            elif isinstance(obstacleObjs[i], AI.bananaPeel):
+                obstacleObjs[i].doBananaPeelAction(p, floorY(), SOCCER_GRAVITY, WINWIDTH)
+                BANANA_IMG_ROT = pygame.transform.rotate(obstacleObjs[i].image, obstacleObjs[i].slipRotate(floorY(), BANANA_ROTATE_FIRST, BANANA_ROTATE_SECOND))            
+                blit_alpha(DISPLAYSURF, BANANA_IMG_ROT, obstacleObjs[i].get_rect(), obstacleObjs[i].doFadeOutBananaPeel(BANANA_PEEL_FADE_DECREMENT))
+                # Has the banana peel faded to 0 after being slipped on?
+                # (This check has been validated)
+                if obstacleObjs[i].getBananaPeelFadeAmount() <= 0:                    
+                    del obstacleObjs[i]            
+            elif isinstance(obstacleObjs[i], AI.spikes):
+                obstacleObjs[i].spikeBump(p)
+                DISPLAYSURF.blit(obstacleObjs[i].image, obstacleObjs[i].get_rect())
+                # Default for drawing any other obstacles
+            else:
+                DISPLAYSURF.blit(obstacleObjs[i].image, obstacleObjs[i].get_rect())
+                
         pygame.display.update()
         FPSCLOCK.tick()
 
